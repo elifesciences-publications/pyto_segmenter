5ab2486bd57536bceaafed4ac7ef43ad990ab81f
nrweir <nicholas.r.weir@gmail.com>
2016-05-13 17:03:40 -0400

    renaming pex_seg and pyto_segment_class; adding __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+''' pyto_segmenter, a package for segmentation of yeast cells.
+
+this package is currently under active development and different pieces may (or
+may not) be functional at any given point in time.
+'''
+
+from CellSegmenter import CellSegmenter, CellSegmentObj
+from PexSegmenter import PexSegmenter, PexSegmentObj
+
+from pyto_segmenter import CellSegmenter, CellSegmentObj
+from pyto_segmenter import PexSegmenter, PexSegmentObj
+
+
